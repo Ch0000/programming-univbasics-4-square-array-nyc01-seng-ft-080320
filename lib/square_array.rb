a2e61bccf88408d30248410e013bc0f051704dbe
Christian P Andrews <cpandrews91@gmail.com>
2020-07-26 10:27:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,25 @@
-def square_array(array)
-  # your code here
+##class table
+##naming class reference
+##def self.square_array(Array[0...3].each{|n| print n, 'n*n'})
+class Table
+def initialize(0)
+  @n = 0
 end
+
+def squares_array
+  Array_1 = [0...3]
+while @n < Array_1.length do
+  squares = n*n
+  puts n
+##  until
+##
+  end
+end
+##def table.square_array
+  puts n
+##end
+
+##def square_array(array)
+##array.each do |i|
+##i**i
+##end
